2e9c02471285939403ae0279db81e8c2fe8468c0
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-04-23 20:21:38 +0200

    added build script

--- build.sh
+++ build.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 sass ./scss/style.scss ./code/public/style.css
+
+DIST_DIR="./dist"
+
+if [ -d "$DIST_DIR" ]; then 
+    rm -r "$DIST_DIR"
+    echo "deleted current dist directory"
+fi
+
+cd code
+npm run build
+
+cd ..
+
+mv ./code/dist ./dist
